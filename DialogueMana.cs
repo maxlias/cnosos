324a3eba5ada59dc1729d420455ee9b02642a4ed
Maximiliano Giuliani <maxgyu@gmail.com>
2017-06-10 18:20:31 -0300

    Dialogue Trigger and Manager Script

---
+++ DialogueMana.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueMana : MonoBehaviour {
+
+    public GameObject dBox;
+    public Text dText;
+    public bool active;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (active && Input.GetKeyDown(KeyCode.E))
+        {
+            dBox.SetActive(false);
+            active = false;
+        }
+	}
+
+    public void ShowBox(string dialogue)
+    {
+        dBox.SetActive(true);
+        active = true;
+        dText.text = dialogue;
+    }
+}
---
+++ DialogueTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour {
+
+    public string dialogue;
+    private DialogueMana dMana;
+
+	// Use this for initialization
+	void Start () {
+        dMana = FindObjectOfType<DialogueMana>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay(Collider hit)
+    {
+        if(hit.gameObject.name == "Character" || hit.gameObject.name == "CharacterOnline")
+        {
+            
+            if(Input.GetKeyUp(KeyCode.E))
+            {
+                
+                dMana.ShowBox(dialogue);
+            }
+        }
+    }
+}
