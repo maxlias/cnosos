8039283bf35e4abff87d93b6e8221d52aec95320
Maximiliano Giuliani <maxgyu@gmail.com>
2017-06-10 18:20:31 -0300

    Added script for chests

---
+++ ChestFound.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestFound : MonoBehaviour {
+
+    
+    private ItemMana iMana;
+    public DialogueMana dMana;
+
+    // Use this for initialization
+    void Start()
+    {
+        iMana = FindObjectOfType<ItemMana>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider hit)
+    {
+        if (hit.gameObject.name == "Character")
+        {
+
+            if (Input.GetKeyUp(KeyCode.E))
+            {
+
+                var chest = hit.gameObject.GetComponent<VariableChest>().getKeyNumber();
+                var thisChest = int.Parse(this.gameObject.name);
+
+                if (thisChest == chest)
+                {
+                    dMana.ShowBox("You found the key! Hurry and escape");
+                    iMana.ShowItem();
+                }
+                else
+                {
+                    dMana.ShowBox("Empty chest");
+                }
+
+            }
+               
+        }
+    }
+}
---
+++ VariableChest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableChest : MonoBehaviour {
+
+    private int chestWithKey;
+	// Use this for initialization
+	void Start () {
+
+        chestWithKey = Random.Range(1, 8);
+        Debug.Log(chestWithKey);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public int getKeyNumber()
+    {
+        return chestWithKey;
+    }
+}
