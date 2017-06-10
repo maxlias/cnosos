7f8f4942b697bc0995312cc5360a41bff61ca600
Maximiliano Giuliani <maxgyu@gmail.com>
2017-06-10 18:20:31 -0300

    Character and Menu script

---
+++ CharacterManagerScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManagerScript : MonoBehaviour {
+
+    private string multiplayerMode;
+    private string playerUnit;
+    public Transform unityChanMultiplayer;
+    public Transform minotaurMultiplayer;
+    public Transform unityChan;
+    public Transform minotaur;
+
+    void Start () {
+
+        multiplayerMode = PlayerPrefs.GetString("Multi");
+        playerUnit = PlayerPrefs.GetString("Player");
+
+        SpawnPlayers();
+    }
+	
+	void Update () {
+		
+	}
+
+    void SpawnPlayers()
+    {
+        var initialUnityChanPos = new Vector3(64.44f, 0.15f, 22.47f);
+        var initialMinotaurPos = new Vector3(-67.44f, -0.27f, -6.51f);
+
+        if (playerUnit == null || playerUnit == "")
+        {
+            PlayerPrefs.SetString("player", "unitychan");
+        }
+
+        if (multiplayerMode == null || multiplayerMode == "")
+        {
+            PlayerPrefs.SetString("Multi", "No");
+            multiplayerMode = "No";
+        }
+
+        if (multiplayerMode == "Yes")
+        {
+            Debug.Log("Entro en si");
+            var multiUnity = Instantiate(unityChanMultiplayer, initialUnityChanPos, transform.rotation);
+            var multiMino = Instantiate(minotaurMultiplayer, initialMinotaurPos, transform.rotation);
+
+            var tutorialController = multiUnity.Find("CharacterOnline").GetComponent(TutorialController);
+        }
+        else
+        {
+            var unity = Instantiate(unityChan, initialUnityChanPos, transform.rotation);
+            var mino = Instantiate(minotaur, initialMinotaurPos, transform.rotation);
+        }
+
+    }
+}
---
+++ Menu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+
+    
+    public Button start;
+    public Button exit;
+
+	// Use this for initialization
+	void Start () {
+
+        start = start.GetComponent<Button>();
+        exit = exit.GetComponent<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartLevel()
+    {
+        PlayerPrefs.SetString("Multi", "No");
+        PlayerPrefs.SetString("Player", "unitychan");
+        Application.LoadLevel(1);
+    }
+
+    public void StartMulitplayer(string player)
+    {
+        PlayerPrefs.SetString("Multi", "Yes");
+        PlayerPrefs.SetString("Player", player);
+        Application.LoadLevel(1);
+    }
+
+    public void QuitLevel()
+    {
+        Application.Quit();    
+    }
+
+    public void Restart()
+    {
+        Application.LoadLevel(0);
+    }
+}
