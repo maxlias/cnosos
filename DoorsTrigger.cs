2e9d932f60a23050eb0af36e5d001d013920cf70
Maximiliano Giuliani <maxgyu@gmail.com>
2017-07-20 19:20:42 -0300

    Added doors trigger script

---
+++ DoorsTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorsTrigger : MonoBehaviour {
+
+    private ItemMana iMana;
+    // Use this for initialization
+    void Start () {
+
+        iMana = FindObjectOfType<ItemMana>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay(Collider hit)
+    {
+        var keyFound = iMana.GetKeyState();
+        if (hit.gameObject.name == "Character" && keyFound)
+        {
+            Application.LoadLevel(3);
+        }
+    }
+}
