15c764d6896de87b2f94b7553a967866a0354681
Maximiliano Giuliani <maxgyu@gmail.com>
2017-09-15 11:20:12 -0300

    Added item manager script

---
+++ ItemMana.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemMana : MonoBehaviour {
+
+    
+    public Image iImage;
+    private bool keyFound = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ShowItem()
+    {
+        iImage.gameObject.SetActive(true);
+        keyFound = true;
+    }
+
+    public bool GetKeyState()
+    {
+        if(keyFound)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
